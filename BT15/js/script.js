6b6f1adabe36782c8e13552dbf8a335923b7af4a
taminhquang13 <135523136+taminhquang13@users.noreply.github.com>
2023-07-28 12:18:38 +0700

    BT15

---
+++ BT15/js/script.js
@@ -0,0 +1,62 @@
+// Bài 1
+var a = 1;
+var b = 2;
+
+a = a + b;
+b = a -b;
+a = a -b;
+
+console.log(a);
+console.log(b);
+
+// Bài 2
+var s = 10 + 20 + 5**10 / 2;
+
+console.log(s);
+
+// Bài 3
+var c = 1;
+var d = 2;
+var e = 3;
+
+if (c > d && c > e) {
+    console.log(c);
+} else if (d > c && d > e) {
+    console.log(d);
+} else if (e > c && e > d) {
+    console.log(e);
+}
+
+// Bài 4
+var f = 1;
+var g = -1;
+
+if (f === g) {
+    console.log(`Hai số cùng dấu`)
+} else {
+    console.log(`Hai số khác dấu`)
+}
+
+// Bài 5 
+var h = 5;
+var i = 10;
+var j = -30;
+
+if (h < i && h < j) {
+    if (i < j) {
+        console.log(h + "," + i + "," + j);
+    } else {
+        console.log(h + "," + j + "," + i);
+    }
+} else if (i < h && i < j) {
+    if (h < j) {
+        console.log(i + "," + h + "," + j);
+    } else {
+        console.log(i + "," + j + "," + h);
+    }
+} else if (j < h && j < i ) {
+    if (h < i) {
+        console.log(j + "," + h + "," + i);
+    } else 
+    console.log(j + "," + i + "," + h);
+}
