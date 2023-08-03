6305f661716ad9db4c9e60d7aef9f20a220b94a5
taminhquang13 <135523136+taminhquang13@users.noreply.github.com>
2023-08-04 00:59:42 +0700

    BT18

--- BT18/js/main.js
+++ BT18/js/main.js
@@ -4,11 +4,11 @@ var arr = text.split(" ");
 var arr2 = text.split(" ");
 var i = 0;
 setInterval(function () {
-  if (i >= arr.length) {
-    i = 0;
-  }
-  if (i < arr.length) {
+  if (i !== 0) {
     arr[i - 1] = arr2[i - 1];
+  }
+  if (i === arr.length) {
+    i = 0;
   }
   arr[i] = `<span style="color: red">${arr[i]}</span> `;
   content.innerHTML = arr.join(" ");
