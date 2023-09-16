1bea01c8f9a9e4b1ef21a84a9042fb435f68396b
taminhquang13 <135523136+taminhquang13@users.noreply.github.com>
2023-09-16 23:58:32 +0700

    BT29

--- BT29/main.js
+++ BT29/main.js
@@ -67,31 +67,16 @@ listItems.forEach(function (item) {
 tableList.addEventListener("dragover", function (e) {
   e.preventDefault();
   var dragging = document.querySelector(".dragging");
-  var nextElement = getDragAfterElement(tableList, e.clientY);
-  console.log(nextElement);
-  if (nextElement == null) {
-    tableList.append(dragging);
+  var notDragging = document.querySelectorAll(".list-items:not(.dragging)");
+  console.log(notDragging.offsetTop);
+  var afterEL = [...notDragging].filter(function (item) {
+    return item.offsetTop > e.clientY;
+  });
+  console.log(afterEL);
+  if (afterEL.length === 0) {
+    tableList.appendChild(dragging);
   } else {
-    tableList.insertBefore(dragging, nextElement);
+    tableList.insertBefore(dragging, afterEL[0]);
   }
 });
 
-function getDragAfterElement(tableList, y) {
-  const draggableElements = [
-    ...tableList.querySelectorAll(".list-items:not(.dragging)"),
-  ];
-
-  return draggableElements.reduce(
-    (closest, child) => {
-      const box = child.getBoundingClientRect();
-      const offset = y - box.top - box.height / 2;
-
-      if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child };
-      } else {
-        return closest;
-      }
-    },
-    { offset: Number.NEGATIVE_INFINITY }
-  ).element;
-}
