335689e0cd3dac3d3b468f4999a810578ae28450
taminhquang13 <135523136+taminhquang13@users.noreply.github.com>
2023-09-26 23:21:04 +0700

    BT32

--- BT32/component.js
+++ BT32/component.js
@@ -27,16 +27,36 @@ class F8 {
 
         console.log(count);
         console.log(title);
+        //style h1
 
         if (results) {
+          //style h1
           var h1 = ShadowRoot.querySelector("h1");
-          var h2 = ShadowRoot.querySelector("h2");
+
+          h1.style.color = "blue";
+          h1.style.fontSize = "50px";
+          h1.style.textAlign = "center";
           h1.innerText = title;
+
+          //style h2
+          var h2 = ShadowRoot.querySelector("h2");
+          h2.style.color = "red";
+          h2.style.fontSize = "30px";
+          h2.style.textAlign = "center";
+          //style button
+
           h2.innerText = count;
         }
 
         const btn = ShadowRoot.querySelectorAll("button");
         btn.forEach((item) => {
+          item.style.padding = "10px";
+          item.style.margin = "10px";
+          item.style.borderRadius = "10px";
+          item.style.backgroundColor = "green";
+          item.style.color = "white";
+          item.style.fontSize = "20px";
+
           const checkClick = item.getAttribute("v-on:click");
           if (checkClick === "count--") {
             item.addEventListener("click", () => {
@@ -91,20 +111,11 @@ F8.component("f8-counter", {
   template: `
           <h1>{{ title }}</h1>
           <h2>Counted: <span class="count-span">{{ count }}</span> times</h2>
+          <div>
           <button v-on:click="count--">Vứt vịt ra đường</button>
-          <button v-on:click="count++">Bỏ vịt vào giỏi</button>
+          <button v-on:click="count++">Bỏ vịt vào giỏ</button>
           <button v-on:click="title">Thanh toán đi bro</button>
           <button v-on:click="f5">Mua lại vịt</button>
-        `,
-});
-
-F8.component("header-component", {
-  data: () => {
-    return {
-      title: "Xưởng bán vịt uy tín của Tạ Minh Quang",
-    };
-  },
-  template: `
-          <h1>{{ title }}</h1>
+          </div>
         `,
 });
